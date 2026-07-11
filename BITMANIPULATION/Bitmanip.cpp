254b879f97921e6fbdf20c1b19c067575fc8aa38
Prabash-295 <prabashvarmamandapati@gmail.com>
2026-07-11 18:55:48 +0530

    implemented all bit manipulation techniques

--- BITMANIPULATION/Bitmanip.cpp
+++ BITMANIPULATION/Bitmanip.cpp
@@ -7,10 +7,57 @@ string checkEvenOrOdd(int n){
   return "even";
 }
 
+// Check Bit is on or not
+string CheckBit(int n, int k){
+  int mask = (1<<k);
+  if(n&mask) return "Bit is on";
+  return "Bit is Off";
+}
+
+
+// Set Bit on
+int SetBit(int n, int k){
+  int mask = (1<<k);
+  return (n|mask);
+}
+
+
+// Clear Bit to off
+int ClearBit(int n, int k){
+  int mask = ~(1<<k);
+  return (n&mask);
+}
+
+
+// Toggle
+int toggleBit(int n, int k){
+  int mask = (1<<k);
+  return (n^mask);
+}
+
+// count set bits
+int countSetBits(int n){
+  int count = 0;
+  while(n)
+    {
+        n = n & (n - 1);
+        count++;
+    }
+    return count;
+}
+
+// swap nibbles
+unsigned char swapNibbles(unsigned char x){
+  unsigned char lower = x&15;
+  unsigned char upper = x&240;
+  lower = lower<<4;
+  upper = upper>>4;
+  unsigned char result = lower | upper;
+  return result;
+}
 
 int main(){
-  int n;
-  cin>>n;
-  cout<<checkEvenOrOdd(n);
+  unsigned char c = 172;
+  cout<<(int)swapNibbles(c);
   return 0;
 }
